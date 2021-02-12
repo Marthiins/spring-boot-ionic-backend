ca960e1f6184fc08102728e8eee9e39f572a8c9f
Marthiins <sergio.marthiins@gmail.com>
2021-02-11 21:43:20 -0300

    Criando operação de instanciação

--- src/main/java/com/marthiins/cursomc/CursomcApplication.java
+++ src/main/java/com/marthiins/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.marthiins.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marthiins.cursomc.domain.Categoria;
+import com.marthiins.cursomc.repositories.CategoriaRepository;
 
+
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; //Categoria repository vai ser o objeto responsavel por salvar as categoria no Banco de Dados
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}/* Implemenar a CommandLineRunner para puxar a função run abaixo */
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria (null, "Informática");
+		Categoria cat2 = new Categoria (null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); //Criar LIsta automatica
+
 	}
 
 }
