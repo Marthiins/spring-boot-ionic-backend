4cd27796aa1256165d89e112b5b5cf7106112039
Marthiins <sergio.marthiins@gmail.com>
2021-02-24 15:24:34 -0300

    Criando o profile de teste

--- src/main/java/com/marthiins/cursomc/CursomcApplication.java
+++ src/main/java/com/marthiins/cursomc/CursomcApplication.java
@@ -1,60 +1,13 @@
 package com.marthiins.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.marthiins.cursomc.domain.Categoria;
-import com.marthiins.cursomc.domain.Cidade;
-import com.marthiins.cursomc.domain.Cliente;
-import com.marthiins.cursomc.domain.Endereco;
-import com.marthiins.cursomc.domain.Estado;
-import com.marthiins.cursomc.domain.ItemPedido;
-import com.marthiins.cursomc.domain.Pagamento;
-import com.marthiins.cursomc.domain.PagamentoComBoleto;
-import com.marthiins.cursomc.domain.PagamentoComCartao;
-import com.marthiins.cursomc.domain.Pedido;
-import com.marthiins.cursomc.domain.Produto;
-import com.marthiins.cursomc.domain.enums.EstadoPagamento;
-import com.marthiins.cursomc.domain.enums.TipoCliente;
-import com.marthiins.cursomc.repositories.CategoriaRepository;
-import com.marthiins.cursomc.repositories.CidadeRepository;
-import com.marthiins.cursomc.repositories.ClienteRepository;
-import com.marthiins.cursomc.repositories.EnderecoRepository;
-import com.marthiins.cursomc.repositories.EstadoRepository;
-import com.marthiins.cursomc.repositories.ItemPedidoRepository;
-import com.marthiins.cursomc.repositories.PagamentoRepository;
-import com.marthiins.cursomc.repositories.PedidoRepository;
-import com.marthiins.cursomc.repositories.ProdutoRepository;
 
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	
-	//Dependencias
-	@Autowired
-	private CategoriaRepository categoriaRepository; //Os repository vai ser o objeto responsavel por salvar os dados no Banco de Dados
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	@Autowired
-	private EstadoRepository estadoRepository;
-	@Autowired
-	private CidadeRepository cidadeRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -64,116 +17,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	
-		Categoria cat1 = new Categoria (null, "Informática");
-		Categoria cat2 = new Categoria (null, "Escritório");
-		Categoria cat3 = new Categoria (null, "Cama mesa e banho");
-		Categoria cat4 = new Categoria (null, "Peças Carro");
-		Categoria cat5 = new Categoria (null, "Eletronicos");
-		Categoria cat6 = new Categoria (null, "Decoração");
-		Categoria cat7 = new Categoria (null, "Perfumaria");
 		
-		
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
-		Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
-		Produto p5 = new Produto(null, "Toalha", 50.00);
-		Produto p6 = new Produto(null, "Colcha", 200.00);
-		Produto p7 = new Produto(null, "TV true color", 1800.00);
-		Produto p8 = new Produto(null, "Roçadeira", 800.00);
-		Produto p9 = new Produto(null, "Abaju", 100.00);
-		Produto p10 = new Produto(null, "Pendente", 180.00);
-		Produto p11 = new Produto(null, "Shampoo", 90.00);
-		
-		//Associação da Categorias aos produtos temos que adicionar 
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
-		
-		//Associar os Produtos as Categorias
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p2.getCategorias().addAll(Arrays.asList(cat1 , cat2, cat4));
-		p3.getCategorias().addAll(Arrays.asList(cat1 , cat4));
-		
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
-		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
-		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
-		cat5.getProdutos().addAll(Arrays.asList(p8));
-		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
-		cat7.getProdutos().addAll(Arrays.asList(p11));
-		
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
-		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p4.getCategorias().addAll(Arrays.asList(cat2));
-		p5.getCategorias().addAll(Arrays.asList(cat3));
-		p6.getCategorias().addAll(Arrays.asList(cat3));
-		p7.getCategorias().addAll(Arrays.asList(cat4));
-		p8.getCategorias().addAll(Arrays.asList(cat5));
-		p9.getCategorias().addAll(Arrays.asList(cat6));
-		p10.getCategorias().addAll(Arrays.asList(cat6));
-		p11.getCategorias().addAll(Arrays.asList(cat7));
-	
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7)); //Criar LIsta automatica para salvar no banco de dados
-        produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11)); // criar a Lista para salvar todos os produtos por enquanto so temos 3
-	
-	
-        Estado est1 = new Estado(null, "Brasilia");
-		Estado est2 = new Estado(null, "Goias");
-		
-		Cidade c1 = new Cidade(null, "Brazlândia", est1);
-		Cidade c2 = new Cidade(null, "Vendinha", est2);
-		Cidade c3 = new Cidade(null, "Monte Alto", est2);
-		
-		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2, c3));
-	
-	    estadoRepository.saveAll(Arrays.asList(est1, est2));
-	    cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-	    
-	    //Instanciar o Cliente
-	    Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-	    cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-	    
-	    //os endereços conhece a cidade e os clientes
-	    Endereco e1 =new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
-	    Endereco e2 =new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-	
-	   //Associar o cliente para conhecer os endreços
-	    cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-	    
-	    clienteRepository.saveAll(Arrays.asList(cli1));
-	    enderecoRepository.saveAll(Arrays.asList(e1 , e2));
-	    
-	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-	    //Estanciando os pedidos
-	    Pedido ped1 = new Pedido(null, sdf.parse("14/02/2021 20:50"), cli1, e1);
-	    Pedido ped2 = new Pedido(null, sdf.parse("15/02/2021 21:50"), cli1, e2);
-	    
-	    Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, null, 6);
-	    ped1.setPagamento(pagto1);
-	    
-	    Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("14/01/2021 00:00"), null);
-	    ped2.setPagamento(pagto2);
-	    
-	    cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-	
-	    pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
-	    pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
-	
-	
-	    ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
-	    ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-	    ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
-	    
-	    ped1.getItens().addAll(Arrays.asList(ip1, ip2));
-	    ped2.getItens().addAll(Arrays.asList(ip3));
-	    
-	    p1.getItens().addAll(Arrays.asList(ip1));
-	    p2.getItens().addAll(Arrays.asList(ip3));
-	    p3.getItens().addAll(Arrays.asList(ip2));
-	    
-	    //Salvar na Base da Dados temos que criar um repository
-	    itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
