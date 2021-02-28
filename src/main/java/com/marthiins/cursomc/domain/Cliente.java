58459732be6d1f0011ac5a020a8fa9861b16ee9b
Marthiins <sergio.marthiins@gmail.com>
2021-02-28 17:33:25 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/marthiins/cursomc/domain/Cliente.java
+++ src/main/java/com/marthiins/cursomc/domain/Cliente.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.marthiins.cursomc.domain.enums.Perfil;
 import com.marthiins.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -48,11 +51,16 @@ public class Cliente implements Serializable {
 	@CollectionTable(name ="TELEFONE")
 	private Set<String> telefones = new HashSet<>(); //uma classe para ser estanciada no set é o HashSet
 
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
 	@JsonIgnore//@JsonBackReference por motivo de erro troquei por @jsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos =new ArrayList<>();
 	
     public Cliente() {
+    	addPerfil(Perfil.CLIENTE);// Para adiconar o perfil cliente como padrão;
     }
  
     //não colocar as coleções nos Construtor com argumentos
@@ -65,6 +73,7 @@ public class Cliente implements Serializable {
 		this.tipo = (tipo==null) ? null : tipo.getCod(); //coloquei o getCod so para pegar o código tipo do cliente depois gerei uma condição se tipo for igual a nulo irei atribuir nulo
 		// caso o contrario atribuo o codigo com o opeardor ternario
 	    this.senha = senha;
+	    addPerfil(Perfil.CLIENTE);// Da mesma forma, add perfil cliente padronizado;
     }
 
 	public Integer getId() {
@@ -114,6 +123,14 @@ public class Cliente implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet()); //Código Lambda para retornar os perfil do cliente
+	}
+
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
 	}
 
 	public List<Endereco> getEnderecos() {
--- src/main/java/com/marthiins/cursomc/services/DBService.java
+++ src/main/java/com/marthiins/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.marthiins.cursomc.domain.PagamentoComCartao;
 import com.marthiins.cursomc.domain.Pedido;
 import com.marthiins.cursomc.domain.Produto;
 import com.marthiins.cursomc.domain.enums.EstadoPagamento;
+import com.marthiins.cursomc.domain.enums.Perfil;
 import com.marthiins.cursomc.domain.enums.TipoCliente;
 import com.marthiins.cursomc.repositories.CategoriaRepository;
 import com.marthiins.cursomc.repositories.CidadeRepository;
@@ -130,15 +131,22 @@ public class DBService {
 	    Cliente cli1 = new Cliente(null, "Maria Silva", "sergio.marthiins@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, passwordEncoder.encode("123"));
 	    cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 	    
+
+		Cliente cli2 = new Cliente(null, "Sergio Marthins", "sergiomartthins@gmail.com", "86563549063", TipoCliente.PESSOAFISICA, passwordEncoder.encode("1234"));
+		cli2.getTelefones().addAll(Arrays.asList("27363325", "996150767"));
+		cli2.addPerfil(Perfil.ADMIN);
+	    
 	    //os endereços conhece a cidade e os clientes
 	    Endereco e1 =new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 	    Endereco e2 =new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+	    Endereco e3 = new Endereco(null, "Avenida das Perdizes", "412", "Casa", "Santa Barbara", "7370000", cli2, c2);
 	
 	   //Associar o cliente para conhecer os endreços
 	    cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+	    cli2.getEnderecos().addAll(Arrays.asList(e3));
 	    
-	    clienteRepository.saveAll(Arrays.asList(cli1));
-	    enderecoRepository.saveAll(Arrays.asList(e1 , e2));
+	    clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+	    enderecoRepository.saveAll(Arrays.asList(e1 , e2, e3));
 	    
 	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 	    //Estanciando os pedidos
