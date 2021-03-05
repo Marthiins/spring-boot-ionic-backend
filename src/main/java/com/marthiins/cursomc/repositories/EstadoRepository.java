26085069c918b7c9cfe756ae96d8b6e866292975
Marthiins <sergio.marthiins@gmail.com>
2021-03-04 23:35:02 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/com/marthiins/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/marthiins/cursomc/repositories/EstadoRepository.java
@@ -1,11 +1,17 @@
 package com.marthiins.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marthiins.cursomc.domain.Estado;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
 
+	@Transactional(readOnly=true)
+	public List<Estado> findAllByOrderByNome();//Retornar todos os estados ordernados por Nome
+	
 }
